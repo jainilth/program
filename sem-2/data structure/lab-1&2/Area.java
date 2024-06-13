8243ffee22e1f8f374b6fe428d7833e96959a92e
@jainil@ <jainilthummar2005@gmail.com>
2024-06-13 22:04:35 +0530

    13-6-24

---
+++ sem-2/data structure/lab-1&2/Area.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("plese enter radious");
+        int a = sc.nextInt();
+        double ans;
+        ans = Math.PI * a * a;
+        System.out.println(ans);
+    }
+}
---
+++ sem-2/data structure/lab-1&2/Vowel.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        c=Character.toUpperCase(c);
+        if (c == 'A' || c == 'E' | c == 'I' || c == 'O' || c == 'U') {
+            System.out.println("vowel");
+        } else {
+            System.out.println("not");
+        }
+    }
+}
