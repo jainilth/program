eed9bccc37a6e34002b24564202538a82e264857
@jainil@ <jainilthummar2005@gmail.com>
2024-08-07 20:48:53 +0530

    7-8-24

--- sem-3/data structure/lab-18/Binery.java
+++ sem-3/data structure/lab-18/Binery.java
@@ -1,28 +1,60 @@
+// import java.util.*;
+// public class Binery {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a[]={1,2,4,6,7,10,13,24,78};
+//         int n=sc.nextInt();
+//         int min=0;
+//         int max=a.length-1;
+//         boolean t=false;
+//         while (min<max) {
+//             int mid=(max+min)/2;
+//             if(a[mid]>n){
+//                 max=mid-1;
+//             }
+//             else if(a[mid]==n){
+//                 System.out.println("the number is available at "+(mid+1)+" position");            
+//                 t=true;
+//                 break;
+//             }
+//             else{
+//                 min=mid+1;
+//             }
+//         }
+//         if(!t){
+//             System.out.println("element is not present");
+//         }
+//     }
+// }
 import java.util.*;
+
 public class Binery {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a[]={1,2,4,6,7,10,13,24,78};
-        int n=sc.nextInt();
-        int min=0;
-        int max=a.length-1;
-        boolean t=false;
-        while (min<max) {
-            int mid=(min+max)/2;
-            if(a[mid]==n){
-                System.out.println("the number is available at "+(mid+1)+" position");            
-                t=true;
+        Scanner sc = new Scanner(System.in);
+        int a[] = {1, 2, 4, 6, 7, 10, 13, 24, 78};
+        System.out.println("Enter the number to search:");
+        int n = sc.nextInt();
+        int min = 0;
+        int max = a.length - 1;
+        boolean t = false;
+
+        while (min <= max) {
+            int mid = (max + min) / 2;
+            if (a[mid] > n) {
+                max = mid - 1;
+            } else if (a[mid] == n) {
+                System.out.println("The number is available at " + (mid + 1) + " position");
+                t = true;
                 break;
-            }
-            else if(a[mid]<n){
-                min=mid+1;
-            }
-            else{
-                max=mid-1;
+            } else {
+                min = mid + 1;
             }
         }
-        if(!t){
-            System.out.println("element is not present");
+
+        if (!t) {
+            System.out.println("Element is not present");
         }
+
+        sc.close();
     }
 }
