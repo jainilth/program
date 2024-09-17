21d2c7b99869e3891c0ade67355ed55f68ef4f84
@jainil@ <jainilthummar2005@gmail.com>
2024-09-17 18:06:27 +0530

    17-9-24

---
+++ sem-3/DBMS/SQL/SQLQuery9.sql
@@ -0,0 +1,40 @@
+--Sub Queries--
+CREATE TABLE Stu_Detail (
+    Rno INT,
+    Name VARCHAR(50),
+    City VARCHAR(50),
+    DID INT,
+);
+
+INSERT INTO Stu_Detail (Rno, Name, City, DID) VALUES
+(101, 'Raju', 'Rajkot', 10),
+(102, 'Amit', 'Ahmedabad', 20),
+(103, 'Sanjay', 'Baroda', 40),
+(104, 'Neha', 'Rajkot', 20),
+(105, 'Meera', 'Ahmedabad', 30),
+(106, 'Mahesh', 'Baroda', 10);
+
+CREATE TABLE Academic (
+    Rno INT,
+    SPI DECIMAL(3, 1),
+    Bklog INT
+);
+
+INSERT INTO Academic (Rno, SPI, Bklog) VALUES
+(101, 8.8, 0),
+(102, 9.2, 2),
+(103, 7.6, 1),
+(104, 8.2, 4),
+(105, 7.0, 2),
+(106, 8.9, 3);
+
+CREATE TABLE Department (
+    DID INT,
+    DName VARCHAR(50)
+);
+
+INSERT INTO Department (DID, DName) VALUES
+(10, 'Computer'),
+(20, 'Electrical'),
+(30, 'Mechanical'),
+(40, 'Civil');
